184e1b0277e53da370327be2f094c8f5886a199b
Elvis Gomez <eogomezf@gmail.com>
2019-03-21 13:58:14 -0600

    Add include to controller and Class createVideo

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -4,7 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+//librerias
+use App\Http\Requests;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\Response;
+
+//Models
+use App\Video;
+use App\Comment;
 class VideoController extends Controller
 {
-    //
+    public function createVideo(){
+        return view('video.createVideo');
+    }
 }
